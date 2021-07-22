99d18b3e733ef22a3be8dace749119ddfa0303d8
retro1235 <82151737+retro1235@users.noreply.github.com>
2021-07-22 13:59:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var image = 
+[
+    "motherpng.jpg",
+    "papap.jpg",
+    "sister.png",
+]
+
+var name = 
+[
+    "Poonam Prasasd",
+    "Daya shankar prasad",
+    "Aayushi",
+]
+
+var i = 0
+function update()
+{
+    i++
+    var numbers_of_family_member_in_array = 3
+    if(i > numbers_of_family_member_in_array)
+}
+ i = 0
+ {
+     var updatedImage = image[i];
+     var updatedname = image[i];
+     document.getElementById("family_member_image").src = updatedImage;
+     document.getElementById("family_member_name").innerHTML = updatedname
+ }
